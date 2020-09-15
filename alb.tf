5c02993a09c5dfbbe938f60a68190d61da778ec4
Alyson Franklin <AlysonFranklinReal@gmail.com>
2020-09-15 10:22:11 -0300

    Criando modulo ecs service

---
+++ alb.tf
@@ -0,0 +1,34 @@
+// target
+resource "aws_alb_target_group" "ecs-service" {
+  name = "${var.TARGET_GROUP_NAME}-${substr(
+    md5(
+      format(
+        "%s%s%s",
+        var.APPLICATION_PORT,
+        var.DEREGISTRATION_DELAY,
+        var.HEALTHCHECK_MATCHER,
+      ),
+    ),
+    0,
+    5,
+  )}"
+  port                          = var.APPLICATION_PORT
+  protocol                      = "HTTP"
+  vpc_id                        = var.VPC_ID
+  deregistration_delay          = var.DEREGISTRATION_DELAY
+  load_balancing_algorithm_type = var.LOAD_BALANCING_ALGORITHM_TYPE
+  tags                          = var.DEFAULT_TAGS
+
+  health_check {
+    healthy_threshold   = var.HEALTHY_THRESHOLD
+    unhealthy_threshold = var.UNHEALTHY_THRESHOLD
+    protocol            = "HTTP"
+    path                = var.HEALTH_CHECK_PATH
+    interval            = var.HEALTH_CHECK_INTERVAL
+    matcher             = var.HEALTHCHECK_MATCHER
+  }
+  stickiness {
+    type    = "lb_cookie"
+    enabled = var.ENABLE_STICKINESS
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,95 @@
+variable "VPC_ID" {
+}
+
+variable "AWS_REGION" {
+}
+
+variable "APPLICATION_NAME" {
+}
+
+variable "APPLICATION_PORT" {
+}
+
+variable "APPLICATION_VERSION" {
+}
+
+variable "CLUSTER_ARN" {
+}
+
+variable "SERVICE_ROLE_ARN" {
+}
+
+variable "DESIRED_COUNT" {
+}
+
+variable "DEPLOYMENT_MINIMUM_HEALTHY_PERCENT" {
+  default = 100
+}
+
+variable "DEPLOYMENT_MAXIMUM_PERCENT" {
+  default = 200
+}
+
+variable "DEREGISTRATION_DELAY" {
+  default = 30
+}
+
+variable "HEALTHCHECK_MATCHER" {
+  default = "200"
+}
+
+variable "UNHEALTHY_THRESHOLD" {
+  default = "3"
+}
+
+variable "HEALTHY_THRESHOLD" {
+  default = "3"
+}
+
+variable "HEALTH_CHECK_INTERVAL" {
+  default = "10"
+}
+
+variable "HEALTH_CHECK_PATH" {
+  default = "/"
+}
+
+variable "LOAD_BALANCING_ALGORITHM_TYPE" {
+  default = "round_robin"
+}
+
+variable "CPU_RESERVATION" {
+}
+
+variable "MEMORY_RESERVATION" {
+}
+
+variable "LOG_GROUP" {
+}
+
+variable "TASK_ROLE_ARN" {
+  default = ""
+}
+
+variable "ALB_ARN" {
+}
+
+variable "ENABLE_STICKINESS" {
+  type    = bool
+  default = false
+}
+
+variable "CREATE_ECR" {
+  description = "Cria repositório ECR ou não?"
+  type        = bool
+}
+
+variable "TARGET_GROUP_NAME" {
+  description = "Nome do target_group"
+  type        = string
+}
+
+variable "DEFAULT_TAGS" {
+  type    = map
+  default = {}
+}
